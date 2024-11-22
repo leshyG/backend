3cfe4bbf81b367934e753809d1bf6441213a3bb0
Leo <leonardo.gonz41@gmail.com>
2024-11-21 22:04:09 -0500

    hostname cambiado al de la nube

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 export const sequelize= new Sequelize('container_db','gonzalo','TKgposH3YKCse4BAIZMlUI8QpniEYxoI',{
-    host: 'localhost',
+    host: 'dpg-csvv4v1opnds73a5ia00-a',
     dialect: 'postgres'
 });
