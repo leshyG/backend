b41fee304f0150f70ca53af0c0212240d7676fb9
Leo <leonardo.gonz41@gmail.com>
2024-12-02 10:02:08 -0500

    agregado url cloud y postgres cloud

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-export const sequelize= new Sequelize('container_db','gonzalo','grupo9.com',{
-    host: 'localhost',
+export const sequelize= new Sequelize('container_db','gonzalo','TKgposH3YKCse4BAIZMlUI8QpniEYxoI',{
+    host: 'dpg-csvv4v1opnds73a5ia00-a',
     dialect: 'postgres'
 });
 
